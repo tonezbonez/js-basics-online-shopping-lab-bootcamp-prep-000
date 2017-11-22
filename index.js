7674ffd83426431f4d34120d2d2748b3bde1d065
Anthony Bracewell <anthonybracewell.violinist@gmail.com>
2017-11-22 04:02:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ var subTotal = 0
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++){
       if (cart[i].hasOwnProperty(item)=== true ){
-        delete cart[item]
+        cart.splice([i],1)
 return cart
       }
   }
