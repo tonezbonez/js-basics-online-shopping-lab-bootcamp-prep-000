308babd8dc62b1b90e4d08ccd87e229087e1457d
Anthony Bracewell <anthonybracewell.violinist@gmail.com>
2017-11-22 04:13:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,5 +69,10 @@ return cart
 
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (Number.isInteger(cardNumber)===true){
+    var totalCost = total()
+    console.log(`Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`)
+  }else{
+    console.log("Sorry, we don't have a credit card on file for you.")
+  }
 }
